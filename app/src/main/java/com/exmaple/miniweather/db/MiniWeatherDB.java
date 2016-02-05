0b8e5965da4a4348bf29e3c533d4437981bf7c58
Sun Zewei <szwnju@gmail.com>
2016-02-05 17:50:53 +0800

    Create DataBase

---
+++ app/src/main/java/com/exmaple/miniweather/db/MiniWeatherDB.java
@@ -0,0 +1,100 @@
+package com.exmaple.miniweather.db;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by apple on 16/2/5.
+ */
+public class MiniWeatherDB {
+
+    private static final String DB_NAME="MiniWeather";
+    private static final int VERSION=1;
+    private SQLiteDatabase db;
+    private static MiniWeatherDB miniWeatherDB;
+
+    public MiniWeatherDB(Context context){
+        MiniWeatherOpenHelper dbHelper=new MiniWeatherOpenHelper(context,DB_NAME,null,VERSION);
+        db=dbHelper.getWritableDatabase();
+    }
+
+    public synchronized static MiniWeatherDB getInstance(Context context){
+        if(miniWeatherDB==null) {
+            miniWeatherDB = new MiniWeatherDB(context);
+        }
+        return miniWeatherDB;
+    }
+
+    public void saveProvince(Province province){
+        ContentValues values=new ContentValues();
+        values.put("provinceName",province.getProvinceName());
+        values.put("provinceCode",province.getProvinceCode());
+        db.insert("Province",null,values);
+    }
+
+    public List<Province> loadProvince(){
+        List<Province> list=new ArrayList<Province>();
+        Cursor cursor=db.query("Province",null,null,null,null,null,null);
+        if(cursor.moveToFirst()){
+            do{
+                Province province=new Province();
+                province.setId(cursor.getInt(cursor.getColumnIndex("id")));
+                province.setProvinceCode(cursor.getString(cursor.getColumnIndex("provinceCode")));
+                province.setProvinceName(cursor.getString(cursor.getColumnIndex("provinceName")));
+
+            }while(cursor.moveToNext());
+        }
+        return list;
+    }
+
+    public void saveCity(City city){
+        ContentValues values=new ContentValues();
+        values.put("cityName",city.getCityName());
+        values.put("cityCode",city.getCityCode());
+        values.put("provinceId",city.getProvinceId());
+        db.insert("City",null,values);
+    }
+
+    public List<City> loadCity(){
+        List<City> list=new ArrayList<City>();
+        Cursor cursor=db.query("City",null,null,null,null,null,null);
+        if(cursor.moveToFirst()){
+            do{
+                City city=new City();
+                city.setCityCode(cursor.getString(cursor.getColumnIndex("cityNode")));
+                city.setCityName(cursor.getString(cursor.getColumnIndex("cityName")));
+                city.setProvinceId(cursor.getInt(cursor.getColumnIndex("provinceId")));
+                list.add(city);
+            }while(cursor.moveToNext());
+        }
+        return list;
+    }
+
+    public void saveCounty(County county){
+        ContentValues values=new ContentValues();
+        values.put("countyName",county.getCountyName());
+        values.put("countyCode",county.getCountyCode());
+        values.put("cityId",county.getCityId());
+        db.insert("County",null,values);
+    }
+
+    public List<County> loadCounty(){
+        List<County> list=new ArrayList<County>();
+        Cursor cursor=db.query("County",null,null,null,null,null,null);
+        if(cursor.moveToFirst()){
+            do{
+                County county=new County();
+                county.setCountyCode(cursor.getString(cursor.getColumnIndex("countyNode")));
+                county.setCountyName(cursor.getString(cursor.getColumnIndex("countyName")));
+                county.setCityId(cursor.getInt(cursor.getColumnIndex("cityId")));
+                list.add(county);
+            }while(cursor.moveToNext());
+        }
+        return list;
+    }
+}
---
+++ app/src/main/java/com/exmaple/miniweather/db/MiniWeatherOpenHelper.java
@@ -0,0 +1,43 @@
+package com.exmaple.miniweather.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by apple on 16/2/5.
+ */
+public class MiniWeatherOpenHelper extends SQLiteOpenHelper {
+
+    private static final String CREATE_PROVINCE="create table Province ("
+            +"id integer primary key autoincrement,"
+            +"province_name text"
+            +"province_code text)";
+
+    private static final String CREATE_CITY="create table City ("
+            +"id integer primary key autoincrement,"
+            +"city_name text"
+            +"city_code text"
+            +"province_id integer)";
+
+    private static final String CREATE_COUNTY="create table County ("
+            +"id integer primary key autoincrement,"
+            +"county_name text"
+            +"county_code text"
+            +"city_id integer)";
+
+    public MiniWeatherOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version){
+        super(context, name, factory, version);
+    }
+    @Override
+    public void onCreate(SQLiteDatabase db){
+        db.execSQL(CREATE_PROVINCE);
+        db.execSQL(CREATE_CITY);
+        db.execSQL(CREATE_COUNTY);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
